83bed98fa4607372b23b2bc654e90e02836630b7
mahdihatami <iz.hatami@gmail.com>
2019-07-09 15:21:29 +0300

    refactoring

--- src/main/java/com/metis/banking/StartApp.java
+++ src/main/java/com/metis/banking/StartApp.java
@@ -5,16 +5,18 @@ import com.metis.banking.domain.Account;
 import com.metis.banking.domain.Amount;
 import com.metis.banking.domain.Statement;
 
-public class StartApp {
+public class StartApp
+{
 
-	public static void main(String[] args) {
-		Account account = Account.createAccount(new Statement());
+    public static void main(String[] args)
+    {
+        Account account = Account.createAccount(new Statement());
 
-		account.deposit(Amount.amountOf(1000), DateCreator.date("10/01/2012"));
-		account.deposit(Amount.amountOf(2000), DateCreator.date("13/01/2012"));
-		account.withdrawal(Amount.amountOf(500), DateCreator.date("14/01/2012"));
+        account.deposit(Amount.amountOf(1000), DateCreator.date("10/01/2012"));
+        account.deposit(Amount.amountOf(2000), DateCreator.date("13/01/2012"));
+        account.withdrawal(Amount.amountOf(500), DateCreator.date("14/01/2012"));
 
-		account.printStatement(System.out);
-	}
+        account.printStatement(System.out);
+    }
 
 }
--- src/main/java/com/metis/banking/builders/DateCreator.java
+++ src/main/java/com/metis/banking/builders/DateCreator.java
@@ -1,15 +1,17 @@
 package com.metis.banking.builders;
 
-import java.util.Date;
-
 import org.jbehave.core.steps.ParameterConverters.DateConverter;
 
-public class DateCreator {
+import java.util.Date;
 
-	public static Date date(String dateString) {
-		DateConverter dateConverter = new DateConverter();
-		Date date = (Date) dateConverter.convertValue(dateString, Date.class);
-		return date;
-	}
+public class DateCreator
+{
+
+    public static Date date(String dateString)
+    {
+        DateConverter dateConverter = new DateConverter();
+        Date date = (Date) dateConverter.convertValue(dateString, Date.class);
+        return date;
+    }
 
 }
